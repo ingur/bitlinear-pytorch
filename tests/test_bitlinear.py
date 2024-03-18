0e747a92b0417492df754ae2e6de493b2c3d8509
ingurv <ingurv99@gmail.com>
2024-03-18 18:28:36 +0100

    fixed module name

--- tests/test_bitlinear.py
+++ tests/test_bitlinear.py
@@ -1,9 +1,9 @@
 import torch
 
-from bitlinear import BitLinear
+from bitlinear_pytorch import BitLinear
 
 
-def test_bitlinear():
+def test_rand_activation():
     model = BitLinear(784, 256, 8)
     x = torch.randn(128, 784)
     y = model(x)
